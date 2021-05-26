3ca1647fb69a38554f16e9ffe40e21a0e89d82b4
Himanshu Tripathi <82304462+him-ht@users.noreply.github.com>
2021-05-26 15:41:31 +0530

    Create file_lookout.ps1

---
+++ file_lookout.ps1
@@ -0,0 +1,49 @@
+$source="C:\temp\source"
+$target="C:\temp\target"
+
+$files = "C:\temp\source\test.txt"
+
+$i = 0
+function get-sleep{
+    Start-Sleep -Seconds 5
+    
+}
+
+while($i -eq 0)
+{
+    $filefound = $true
+    foreach ($file in $files)
+    {
+
+       if($filefound -eq $false)
+       {
+
+       }
+       else
+       {
+
+            if(Test-Path $file)
+            {
+                $filefound = $true
+            }
+            else
+            {
+                $filefound = $false
+                "$file not found"
+                get-sleep
+            }
+        }
+
+
+    }
+    
+    if($filefound -eq $true)
+    {
+        $i = 1
+
+    }
+
+}
+
+######### Your operation comes here ##########
+Move-Item "$source\test.txt" $target 
